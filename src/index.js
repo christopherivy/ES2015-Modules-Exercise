976fd6aad7e6cb6aee7bc2bd5811452c172f9d41
christopherivy <cdi.747@gmail.com>
2022-03-14 17:52:49 -0700

    ES2015-Modules-Exercise

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import foods from './foods';
+import { choice, remove } from './helpers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let food = choice(foods);
+
+console.log(`I'd like one ${food}, please.`);
+console.log(`Here you go: ${food}`);
+console.log(`Delicious! May I have another ${food} ?`);
+
+let fruitRemaing = remove(food, foods);
+
+remove(food, foods);
+
+console.log(`I'm sorry, we are all out of ${food} . We only have ${fruitRemaing.length} left.`);
